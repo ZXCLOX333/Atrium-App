2b78decc6ef907883eae1c9c16b17400fd02eb8c
ZXCLOX333 <sashakostyrko8@gmail.com>
2025-12-03 00:42:08 +0200

    first screan + new style.py

--- Style.py
+++ Style.py
@@ -1,39 +1,54 @@
 import customtkinter as ctk
-from PIL import Image
+from PIL import Image, ImageFont
 import os
-#File path
-current_path = os.path.dirname(os.path.realpath(__file__))
 
-# Colors
-White = "#FFFFFF"
-LightGray = "#DAD8D8"
-Gold = "#FF8F00"
-DarkGray="#717171"
-Black = "#000000"
+class AppStyles:
+    White = "#FFFFFF"
+    LightGray = "#DAD8D8"
+    Gold = "#FF8F00"
+    DarkGray = "#717171"
+    Black = "#000000"
 
-#Gradients
-_rawBgGradient = Image.open(os.path.join(current_path, "Imgs", "BgGradient.png"))
-_rawBtnEnter = Image.open(os.path.join(current_path, "Imgs", "BtnEnter.png"))
-_rawBtnBuy = Image.open(os.path.join(current_path, "Imgs", "BtnBuy.png"))
-_rawBtnJoin = Image.open(os.path.join(current_path, "Imgs", "BtnJoin.png"))
+    w = 375
+    h = 812
 
-BgGradient = ctk.CTkImage(light_image=_rawBgGradient , dark_image=_rawBgGradient , size=(375, 812))
-BtnEnter = ctk.CTkImage(light_image=_rawBtnEnter , dark_image=_rawBtnEnter , size=(327, 55))
-BtnBuy = ctk.CTkImage(light_image=_rawBtnBuy , dark_image=_rawBtnBuy , size=(327, 55))
-BtnJoin = ctk.CTkImage(light_image=_rawBtnJoin , dark_image=_rawBtnJoin , size=(327, 55))
+    mw = w/2
+    mh = h/2
 
-# Fonts
-font_path_Montserrat = os.path.join(current_path, "fonts", "Montserrat-Regular.ttf")
-font_path_GreatVibes = os.path.join(current_path, "fonts", "GreatVibes-Regular.ttf")
-font_path_Cinzel = os.path.join(current_path, "fonts", "Cinzel-Regular.ttf")
+    def __init__(self):
+        self.current_path = os.path.dirname(os.path.realpath(__file__))
+        
+        self._load_fonts()
+        self._load_images()
 
-ctk.FontManager.load_font(font_path_Montserrat)
-ctk.FontManager.load_font(font_path_GreatVibes)
-ctk.FontManager.load_font(font_path_Cinzel)
+    def _load_fonts(self):
+        font_path_Montserrat = os.path.join(self.current_path, "fonts", "Montserrat-Regular.ttf")
+        font_path_GreatVibes = os.path.join(self.current_path, "fonts", "GreatVibes-Regular.ttf")
+        self.font_path_Cinzel = os.path.join(self.current_path, "fonts", "Cinzel-Bold.ttf")
 
-H1 = ctk.CTkFont(family="Great Vibes", size=32)
-H2 = ctk.CTkFont(family="Montserrat", size=22)
-H3 = ctk.CTkFont(family="Cinzel", size=30)
+        ctk.FontManager.load_font(font_path_Montserrat)
+        ctk.FontManager.load_font(font_path_GreatVibes)
+        ctk.FontManager.load_font(self.font_path_Cinzel)
 
-Body = ctk.CTkFont(family="Montserrat", size=16)
-Crumbs = ctk.CTkFont(family="Montserrat", size=12)
+        self.H1 = ctk.CTkFont(family="Great Vibes", size=32)
+        self.H2 = ctk.CTkFont(family="Montserrat", size=22)
+        self.H3 = ctk.CTkFont(family="Cinzel", size=30)
+        self.Body = ctk.CTkFont(family="Montserrat", size=16)
+        self.Crumbs = ctk.CTkFont(family="Montserrat", size=12)
+
+        self.H1_Pil = ImageFont.truetype(font_path_GreatVibes,  34)
+        self.H2_Pil = ImageFont.truetype(font_path_Montserrat, 22)
+        self.H3_Pil = ImageFont.truetype(self.font_path_Cinzel, 30)
+        self.Body_Pil = ImageFont.truetype(font_path_Montserrat, 16)
+        self.Crumbs_Pil =  ImageFont.truetype(font_path_Montserrat, 12)
+
+    def _load_images(self):
+        self.rawBgGradient = Image.open(os.path.join(self.current_path, "Imgs", "Bg.png"))
+        self.rawBtnEnter = Image.open(os.path.join(self.current_path, "Imgs", "BtnEnter.png"))
+        self.rawBtnBuy = Image.open(os.path.join(self.current_path, "Imgs", "BtnBuy.png"))
+        self.rawBtnJoin = Image.open(os.path.join(self.current_path, "Imgs", "BtnJoin.png"))
+
+        self.BgGradient = ctk.CTkImage(light_image=self.rawBgGradient, dark_image=self.rawBgGradient, size=(375, 812))
+        self.BtnEnter = ctk.CTkImage(light_image=self.rawBtnEnter , dark_image=self.rawBtnEnter , size=(327, 55))
+        self.BtnBuy = ctk.CTkImage(light_image=self.rawBtnBuy , dark_image=self.rawBtnBuy , size=(327, 55))
+        self.BtnJoin = ctk.CTkImage(light_image=self.rawBtnJoin , dark_image=self.rawBtnJoin , size=(327, 55))
--- WelcomeScrean.py
+++ WelcomeScrean.py
@@ -1,2 +1,30 @@
 import customtkinter as ctk
 import Style as st
+from PIL import Image, ImageDraw, ImageFont
+
+class WelcomeScreen(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.assets = st.AppStyles()
+
+        self.geometry("375x812")
+        self.title("Atrium")
+        
+        Cizel65 = ImageFont.truetype(self.assets.font_path_Cinzel, 65)
+
+        bg = self.assets.rawBgGradient.copy()
+        draw = ImageDraw.Draw(bg)
+        draw.text((self.assets.mw+30, 207), "Wellcome to", fill=self.assets.White, font=self.assets.H1_Pil,anchor="mt")
+        draw.text((self.assets.mw+30, 315), "ATRIUM", fill=self.assets.Gold, font=Cizel65,anchor="mt")
+        draw.text((self.assets.mw+30, 430), "To your private collection.", fill=self.assets.White, font=self.assets.H2_Pil,anchor="mt")
+        draw.text((self.assets.mw+30, 465), "Cinema as a privilege.", fill=self.assets.White, font=self.assets.H2_Pil ,anchor="mt")
+        bg_image = ctk.CTkImage(light_image=bg, size=(375, 812))
+        self.background = ctk.CTkLabel(self,text="", image=bg_image)
+        self.background.pack()
+
+        self.BtnEnter = ctk.CTkButton(self, text="", image=self.assets.BtnEnter, hover=False, command=1, border_width=0, width=327, height=55, fg_color="#040D20", corner_radius=0)
+        self.BtnEnter.place(y=770, x=self.assets.mw, anchor="center")
+
+if __name__ == "__main__":
+    app = WelcomeScreen()
+    app.mainloop()
